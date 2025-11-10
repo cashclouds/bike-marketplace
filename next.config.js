8c16be0fc4007d9664136d1b3813a3f1e29646c5
Your Name <your@email.com>
2025-11-10 12:07:52 +0200

    Fix Vercel build: exclude backend from Next.js compilation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      { module: /compression/ },
+      { module: /express/ },
+    ];
+    return config;
+  },
 }
 
 module.exports = nextConfig
